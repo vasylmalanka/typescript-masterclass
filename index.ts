5c5c032eeef0b161842acbee73296c14c3894f2a
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-04 23:17:11 +0200

    1.5

--- index.ts
+++ index.ts
@@ -1,1 +1,15 @@
-console.log("Hello, World!");
+const students = [
+    {
+        name: "John",
+        score: 70,
+    },
+    {
+        name: "Mark",
+        score: 90,
+    },
+];
+
+for (const student of students) {
+    console.log("Name:", student.name);
+    console.log("Score:", student.grade);
+}
