745a83d5057305dfd2a73ed4b6ec19805d3a0f9d
Vasyl Malanka <v.malanka@comwrap.reply.com>
2025-08-05 09:42:23 +0200

    2.11

--- index.ts
+++ index.ts
@@ -1,23 +1,15 @@
-/**
- * String types
- */
-var firstname: string = "John";
-firstname = 1;
-let automobile = "BMW";
-const city = "New York";
-let students = 32;
-let studentsAsString: string = students.toString();
+let user: undefined;
+// console.log(user);
 
-/** Number types */
-var age: number = 32;
-let year: number = 2024;
-const numberOfMembers = 61;
-numberOfMembers = 128;
-let stringToNumber: number = parseInt("1985");
+let userRole: null;
+userRole = null;
+// console.log(userRole);
 
-/**
- * Booleans
- */
-let isStudent: boolean = false;
-const alwaysStudent = true;
-let minimumAge: boolean = age >= 6 ? true : false;
+console.log(userRole === user);
+if (!userRole) {
+  console.log("This condition is true")
+}
+
+if (!user) {
+  console.log("This condition is true")
+}
