343428e772e1dd7fa3f1adbce2d17ad3b611a7ba
Vasyl Malanka <v.malanka@comwrap.reply.com>
2025-08-05 09:31:59 +0200

    2.10

--- index.ts
+++ index.ts
@@ -1,6 +1,23 @@
-function addNumbers(a: number, b: number) {
-  return a + b;
-}
+/**
+ * String types
+ */
+var firstname: string = "John";
+firstname = 1;
+let automobile = "BMW";
+const city = "New York";
+let students = 32;
+let studentsAsString: string = students.toString();
 
-console.log(addNumbers(2, 3));
-// console.log(addNumbers("text", 3));
+/** Number types */
+var age: number = 32;
+let year: number = 2024;
+const numberOfMembers = 61;
+numberOfMembers = 128;
+let stringToNumber: number = parseInt("1985");
+
+/**
+ * Booleans
+ */
+let isStudent: boolean = false;
+const alwaysStudent = true;
+let minimumAge: boolean = age >= 6 ? true : false;
