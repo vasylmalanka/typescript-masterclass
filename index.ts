e710842bddd81c63cd296f4eca77b8efa11df085
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-06 18:36:14 +0200

    3.23

--- index.ts
+++ index.ts
@@ -1,6 +1,14 @@
-type CustomDate = Date;
-type CustomString = string;
+type check = any extends unknown ? true : false;
+type check2 = string extends any ? true : false;
+type check3 = null extends any ? true : false;
+type check4 = undefined extends void ? true : false;
+type check5 = [] extends Object ? true : false;
+type check6 = Function extends Object ? true : false;
 
-type TrueString = CustomString extends string ? true : false;
-type ConditionalNumber = CustomDate extends Date ? number : string;
-type DateAssignment = CustomDate extends Date ? Date : undefined;
+// A function declaration that never completes returns a never type
+const throwError = (errMessage: string) => {
+  throw new Error(errMessage);
+}
+
+let strings: Object = ["a", "b"];
+let myFunct: Object = () => 2;
