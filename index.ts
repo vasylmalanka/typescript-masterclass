1bc72dd31189476bffa45f543f75cc1d13128610
Vasyl Malanka <v.malanka@comwrap.reply.com>
2025-08-06 09:14:46 +0200

    3.17

--- index.ts
+++ index.ts
@@ -1,10 +1,8 @@
-/**
- * Assign the correct types to each of the following variables
- */
-let message: string = "Hello, TypeScript!";
-let age: number = 42;
-let isStudent: boolean = true;
-let fetched: null = null;
-let user: undefined = undefined;
-let largeNumber: bigint = 9007199254740991n;
-let unique: symbol = Symbol("uniqueSymbol");
+let firstname: any = "Mark";
+
+firstname = 123;
+firstname = [];
+
+function returnParam (param) {
+  return param;
+}
