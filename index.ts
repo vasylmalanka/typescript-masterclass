460493452794af005519fe8e015b29dbd2c6757d
Vasyl Malanka <v.malanka@comwrap.reply.com>
2025-08-06 09:19:59 +0200

    3.18

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
-let firstname: any = "Mark";
-
-firstname = 123;
-firstname = [];
-
-function returnParam (param) {
-  return param;
+function multiplyByTwo (number: unknown) {
+  if (typeof number === "number") {
+    return number * 2;
+  }
+  return "Please provide a valid number";
 }
+
+console.log(multiplyByTwo(4));
+console.log(multiplyByTwo("string"));
