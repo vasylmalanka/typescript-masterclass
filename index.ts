64d4795908c846a5f56cb7e39a711e3b8432b518
Vasyl Malanka <v.malanka@comwrap.reply.com>
2025-08-06 23:33:19 +0200

    4.31

--- index.ts
+++ index.ts
@@ -4,11 +4,21 @@ type Author = {
   email: string;
 }
 
+type AwardDetails = {
+  name: string;
+  date: Date;
+}
+
+type Awards = {
+  [key: string]: AwardDetails;
+};
+
 type Post = {
   title: string;
   content: string;
   date: Date;
   author: Author;
+  awards: Awards;
 };
 
 let post: Post = {
@@ -19,5 +29,15 @@ let post: Post = {
     name: "John",
     age: 22,
     email: "john@doe.com",
+  },
+  awards: {
+    web: {
+      name: "Web Awards",
+      date: new Date(),
+    },
+    web3: {
+      name: "Web 3",
+      date: new Date(),
+    },
   },
 };
