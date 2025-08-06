a9b23639f7ed1f0afc1ca03fe7b07ff90574d447
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-06 18:42:45 +0200

    3.24

--- index.ts
+++ index.ts
@@ -1,14 +1,19 @@
-type check = any extends unknown ? true : false;
-type check2 = string extends any ? true : false;
-type check3 = null extends any ? true : false;
-type check4 = undefined extends void ? true : false;
-type check5 = [] extends Object ? true : false;
-type check6 = Function extends Object ? true : false;
+let firstName = <any>"Mark";
+let lastName = "Doe" as any;
 
-// A function declaration that never completes returns a never type
-const throwError = (errMessage: string) => {
-  throw new Error(errMessage);
+// User from API
+let user = {
+  name: "Mark",
+  email: "mark@email.com",
+};
+
+type User = {
+  name: string,
+  email: string,
+};
+
+function fetchUser() {
+  return user as User;
 }
 
-let strings: Object = ["a", "b"];
-let myFunct: Object = () => 2;
+const fetchedUser = fetchUser();
