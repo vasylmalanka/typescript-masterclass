c098477684a115f367d24b83f7c9ade86fc5f707
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-07 14:10:34 +0200

    4.33

--- index.ts
+++ index.ts
@@ -1,71 +1,29 @@
-type Author = {
+type Dog = {
   name: string;
-  age: number;
-  email: string;
-  readonly type: "human" | "ai";
+  barks: boolean;
+  wags: boolean;
+};
+
+type Cat = {
+  name: string;
+  purrs: boolean;
+};
+
+type DogAndCatUnion = Dog | Cat;
+
+let dog: DogAndCatUnion = {
+  name: "Buddy",
+  barks: true,
+  wags: true,
+};
+
+let cat: DogAndCatUnion = {
+  name: "Bella",
+  purrs: true,
 }
 
-type AwardDetails = {
-  name: string;
-  date: Date;
+let hybridAnimal: DogAndCatUnion = {
+  name: "Bella",
+  purrs: true,
+  barks: true,
 }
-
-type Awards = {
-  [key: string]: AwardDetails;
-};
-
-type Post = {
-  title: string;
-  content: string;
-  date: Date;
-  author: Author;
-  awards: Awards;
-  category?: string;
-};
-
-let post: Post = {
-  title: "This is a blog post",
-  content: "Content of the post",
-  date: new Date(),
-  author: {
-    name: "John",
-    age: 22,
-    email: "john@doe.com",
-    type: "human",
-  },
-  awards: {
-    web: {
-      name: "Web Awards",
-      date: new Date(),
-    },
-    web3: {
-      name: "Web 3",
-      date: new Date(),
-    },
-  },
-  category: "javascript",
-};
-
-let post2: Post = {
-  title: "This is a blog post",
-  content: "Content of the post",
-  date: new Date(),
-  author: {
-    name: "John",
-    age: 22,
-    email: "john@doe.com",
-    type: "human",
-  },
-  awards: {
-    web: {
-      name: "Web Awards",
-      date: new Date(),
-    },
-    web3: {
-      name: "Web 3",
-      date: new Date(),
-    },
-  },
-};
-
-post.author.type = "ai";
