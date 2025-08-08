cf4ca889f62df01c29ed31c0e91d8d33925ec5ea
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-08 19:34:51 +0200

    6.49

--- index.ts
+++ index.ts
@@ -1,25 +1,14 @@
-// Practice Questions
-//* 1. Create an array numbers that only accepts numbers and another array strings that only accepts strings.
-let numbers: number[] = [1, 2, 3];
-let strings: string[] = ['a', 'b'];
-
-//* 2. Create a tuple person that holds a string (name) and a number (age).
-let person: [name: string, age: number] = ['Alice', 30];
-
-//* 3. Create a readonly array colors that holds strings and a readonly tuple point that holds two numbers (x, y). Attempt to modify their elements and observe the TypeScript error.
-const colors: readonly string[] = ['red', 'green', 'blue'];
-const point: readonly [number, number] = [10, 20];
-
-//* 4. Create an enum called StatusEnum that should 3 properties Active, Inactive, Pending
-enum StatusEnum {
-  Active = 'active',
-  Inactive = 'inactive',
-  Pending = 'pending',
+// named function
+function intro(name: string, age: number): string {
+  return `My name is ${name} and I am ${age} years old`;
 }
 
-//* 5. Create an object as const called Status with the same structure as an StatusEnum
-let Status = {
-  Active: 'active',
-  Inactive: 'inactive',
-  Pending: 'pending',
-} as const;
+// Function expression
+const intro2 = function (name: string, age: number): string {
+  return `My name is ${name} and I am ${age} years old`;
+};
+
+// Arrow function
+const intro3 = (name: string, age: number): string | number => {
+  return 1234;
+};
