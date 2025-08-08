579837f8db708e8cc28e8333365a90b7fd17cb5f
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-08 12:48:13 +0200

    5.42

--- index.ts
+++ index.ts
@@ -1,15 +1,52 @@
-let numbers: readonly number[] = [1, 2, 3];
+const STATUS_LOADING = "loading";
+const STATUS_STOPPED = "stopped";
 
-numbers.push(2);
+// STATUS_LOADING = "stopped";
 
-type ReadOnlyTuple = readonly [string, string, number];
+enum Direction {
+  Up, // 0
+  Down, // 1
+  Left, // 2
+  Right, // 3
+}
 
-let person: ReadOnlyTuple = ["John", "Doe", 21];
-person[0] = "Mark";
+console.log(Direction.Up);
+Direction.Left = 5;
 
-// array
-type a = Readonly<string[]>;
-type b = ReadonlyArray<(string | number)>
+enum Direction2 {
+  Up = 1, // 1
+  Down, // 2
+  Left, // 3
+  Right, // 4
+};
 
-// tuple
-type c = Readonly<[number, string, number]>;
+console.log(Direction2.Right);
+
+enum Roles {
+  ADMIN = "admin",
+  AUTHOR = "author",
+  EDITOR = "editor",
+};
+
+type Person = {
+  name: string;
+  email: string;
+  password: string;
+  role: Roles;
+};
+
+const person: Person = {
+  name: "John",
+  email: "john@email.com",
+  password: "123df",
+  role: Roles.ADMIN,
+};
+
+console.log(person);
+
+enum Direction3 {
+  Up = 1, // 1
+  Down = "down", // 2
+  Left = 3, // 3
+  Right, // 4
+};
