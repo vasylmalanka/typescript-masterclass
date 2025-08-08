81d85fba3dd9bd8c083372da7bda74239e9f6a77
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-08 13:11:32 +0200

    5.44

--- index.ts
+++ index.ts
@@ -1,52 +1,24 @@
-const STATUS_LOADING = "loading";
-const STATUS_STOPPED = "stopped";
-
-// STATUS_LOADING = "stopped";
-
 enum Direction {
   Up, // 0
   Down, // 1
   Left, // 2
   Right, // 3
-}
-
-console.log(Direction.Up);
-Direction.Left = 5;
-
-enum Direction2 {
-  Up = 1, // 1
-  Down, // 2
-  Left, // 3
-  Right, // 4
 };
 
-console.log(Direction2.Right);
-
-enum Roles {
-  ADMIN = "admin",
-  AUTHOR = "author",
-  EDITOR = "editor",
+const enum EDirection {
+  Up, // 0
+  Down, // 1
+  Left, // 2
+  Right, // 3
 };
 
-type Person = {
-  name: string;
-  email: string;
-  password: string;
-  role: Roles;
-};
+const ODirection = {
+  Up: 0,
+  Down: 1,
+  Left: 2,
+  Right: 3,
+} as const;
 
-const person: Person = {
-  name: "John",
-  email: "john@email.com",
-  password: "123df",
-  role: Roles.ADMIN,
-};
-
-console.log(person);
-
-enum Direction3 {
-  Up = 1, // 1
-  Down = "down", // 2
-  Left = 3, // 3
-  Right, // 4
-};
+let eDirection = EDirection.Up;
+let direction = Direction.Right;
+ODirection.Up = "newValue";
