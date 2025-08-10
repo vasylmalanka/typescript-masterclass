64c9f1c84fd30c260449fcd07b9f4a8f163ab00f
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-10 19:36:02 +0200

    7.72

--- index.ts
+++ index.ts
@@ -1,28 +1,30 @@
-type Filter = {
-  (array: number[], predicate: (item: number) => boolean): number[];
-  (array: string[], predicate: (item: string) => boolean): string[];
-  (array: object[], predicate: (item: object) => boolean): object[];
-}
+const map = <T, U>(array: T[], mapFunction: (item: T) => U): (U | T)[] => {
+  if (array.length === 0) {
+    return array;
+  }
 
-const filter = <T>(array: T[], predicate: (item: T) => boolean): T[] => {
-  let result: T[] = [];
+  let result: U[] = [];
+
   for (let i = 0; i < array.length; i++) {
-    if (predicate(array[i])) {
-      result.push(array[i]);
-    }
+    result.push(mapFunction(array[i]));
   }
   return result;
 };
 
 let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-function predicate(item: number) {
-  return item > 7;
+function addFour(item: number) {
+  return item + 4;
 }
 
-let animals = ['cat', 'dog', 'rat'];
-function filterCat(item: string) {
-  return item === 'cat';
+let animals = ['cat ', ' dog', ' rat   '];
+function trimSpaces(item: string) {
+  return item.trim();
 }
 
-console.log(filter(numbers, predicate));
-console.log(filter(animals, filterCat));
+function convertToString(item: number) {
+  return item.toString();
+}
+
+console.log(map(numbers, addFour));
+console.log(map(animals, trimSpaces));
+console.log(map(numbers, convertToString));
