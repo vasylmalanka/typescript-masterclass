6d77d719af892c8d25e8c2c1e0511bd563ad7e67
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-10 16:43:27 +0200

    7.62

--- index.ts
+++ index.ts
@@ -1,41 +1,18 @@
-/**
- * Practice Excercise for functions
- */
-
-//* 1. Declare a function named greet that takes a string parameter name and returns a greeting message.
-function greet (name: string): string {
-  return `Hello, ${name}!`;
+function returnParams<Type>(param: Type): Type {
+  return param;
 }
 
-//* 2. Define an type Product with properties id (number) and name (string). Create a function named getProduct that takes an id parameter and returns a Product.
-type Product = {
-  id: number;
-  name: string;
+const return1 = returnParams<string>('123');
+const return2 = returnParams<number>(123);
+
+const myParam: <T>(param: T) => T = (param) => param;
+
+const myParam2 = function <U>(param: U): U {
+  return param;
 };
 
-function getProduct(id: number): Product
-{
-  return {
-    id: id,
-    name: 'Example Product 1',
-  };
-}
-
-//* 3. Declare a function signature named Calculator as a type that takes two numbers and returns a number. Implement two functions add and subtract that match this signature.
-type Calculator = (a: number, b: number) => number;
-
-const add: Calculator = (a: number, b: number): number => {
-  return a + b;
-};
-const subtract: Calculator = (a: number, b: number): number => {
-  return a - b;
+type ObjectType = {
+  myParam: <V, X>(param: V, param2: X) => V | X;
 };
 
-//* 4. Create a function named logMessage that takes a string message and logs it to the console, returning void. Also, create a function named throwError that takes a string message and throws an error, returning never.
-function logMessage(message: string): void {
-  console.log(message);
-}
-
-function throwError(message: string): never {
-  throw new Error(message);
-}
+type MyParam = <K>(param: K) => K;
