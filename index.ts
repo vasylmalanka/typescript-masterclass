c467ffb4d18785822945a569fdd1e9f631ce68b3
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-10 17:29:22 +0200

    7.64

--- index.ts
+++ index.ts
@@ -21,3 +21,17 @@ const firstElementNumber: FirstElement<number> = (arr) => {
 };
 
 firstElement(['123']);
+
+type HasLength = {
+  length: number;
+};
+
+function logLength<T extends HasLength>(item: T): void {
+  console.log(item.length);
+}
+
+logLength(numbersArray);
+logLength(stringArray);
+logLength('stringArray');
+
+logLength({ name: 'John', length: 12 });
