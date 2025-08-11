2a052b02c0b3c3fed22fa23c8114bf9130ad0886
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-11 18:16:48 +0200

    8.76

--- index.ts
+++ index.ts
@@ -2,14 +2,17 @@ class User {
   name = 'John';
   email = 'john@email.com';
 
+  constructor(name: string, email: string) {
+    console.log(name);
+    console.log(email);
+  }
+
   greet() {
     return 'Hello John';
   }
 }
 
-const user = new User();
-const user2 = new User();
+const user = new User('John', 'john@email.com');
+const user2 = new User('Mark', 'mark@email.com');
 
 console.log(user);
-console.log(user2);
-console.log(user.greet());
