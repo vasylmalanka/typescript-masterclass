0e74eefa5ead813f381838e7a21ef9b93c5195de
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-11 18:13:06 +0200

    8.75

--- index.ts
+++ index.ts
@@ -1,30 +1,15 @@
-const map = <T, U>(array: T[], mapFunction: (item: T) => U): (U | T)[] => {
-  if (array.length === 0) {
-    return array;
+class User {
+  name = 'John';
+  email = 'john@email.com';
+
+  greet() {
+    return 'Hello John';
   }
-
-  let result: U[] = [];
-
-  for (let i = 0; i < array.length; i++) {
-    result.push(mapFunction(array[i]));
-  }
-  return result;
-};
-
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-function addFour(item: number) {
-  return item + 4;
 }
 
-let animals = ['cat ', ' dog', ' rat   '];
-function trimSpaces(item: string) {
-  return item.trim();
-}
+const user = new User();
+const user2 = new User();
 
-function convertToString(item: number) {
-  return item.toString();
-}
-
-console.log(map(numbers, addFour));
-console.log(map(animals, trimSpaces));
-console.log(map(numbers, convertToString));
+console.log(user);
+console.log(user2);
+console.log(user.greet());
