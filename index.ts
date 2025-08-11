96fb45163371b669cd98cf799389c10c58fd1b46
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-11 18:26:37 +0200

    8.79

--- index.ts
+++ index.ts
@@ -1,10 +1,12 @@
 class User {
   name: string;
-  email: string;
+  readonly email: string;
+  lastname?: string;
 
-  constructor(name: string, email: string) {
+  constructor(name: string, email: string, lastname?: string) {
     this.name = name;
     this.email = email;
+    this.lastname = lastname;
   }
 
   greet(): string {
@@ -14,11 +16,14 @@ class User {
 
 const user: User = new User('John', 'john@email.com');
 const user2: User = new User('Mark', 'mark@email.com');
+const user3: User = new User('Alice', 'alice@email.com', 'Doe');
 
 user.lastname = 'last';
 user.name = 123;
+user.email = 'new@email.com';
 
 console.log(user);
 console.log(user.greet());
 console.log(user2);
 console.log(user2.greet());
+console.log(user3);
