4dacc2020127a61f2c1191a62c597e2575d7efdb
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-13 15:18:19 +0200

    8.92

--- index.ts
+++ index.ts
@@ -1,19 +1,28 @@
 class Person {
-  public fullname: string;
+  private _age?: number;
 
   constructor(
     public firstName: string,
     public lastName: string,
-    public age: number,
   ) {
+  }
+
+  public set age(age: number) {
     if (age > 200 || age < 0) {
       throw new Error('The age must be a within the age range 0-200');
     }
-    this.fullname = `${this.firstName} ${this.lastName}`;
+    this._age = age;
+  }
+
+  public fullname() {
+    return `${this.firstName} ${this.lastName}`;
   }
 }
 
-const john: Person = new Person('John', 'Doe', 45);
-const mark: Person = new Person('Mark', 'Doe', 20);
+const john: Person = new Person('John', 'Doe');
+const mark: Person = new Person('Mark', 'Doe');
+john.age = 45;
+console.log(john.age);
 
-console.log(john.fullname);
+console.log(john.fullname());
+console.log(mark.fullname());
