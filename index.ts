b280cf83abf8b5a396bcf453c3c4c7d923ecf76d
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-14 18:57:04 +0200

    8.102

--- index.ts
+++ index.ts
@@ -1,30 +1,65 @@
-type Constructor = new(...args: any[]) => {};
+class Employee {
+  private static companyName: string;
 
-function TimeStamp<T extends Constructor>(Base: T) {
-  return class extends Base {
-    protected timestamp: Date = new Date();
+  protected readonly id: number;
+  private _salary: number | undefined;
 
-    getTimestamp() {
-      return this.timestamp;
+  constructor(
+    public readonly name: string,
+    public readonly age: number,
+    id: number,
+  ) {
+    this.id = id;
+  }
+
+  set salary(salary: number) {
+    if (salary < 0) {
+      throw new Error('Salary must be a positive number');
     }
-  };
-}
-
-class User {
-  constructor(public name: string) {}
-}
-
-class UserWithTimestamp extends TimeStamp(User) {
-  constructor(name: string, public age: number) {
-    super(name);
+    this._salary = salary;
   }
 
-  displayInfo() {
-    console.log(`Name: ${this.name}, Age: ${this.age}`);
-    console.log(`Timestamp: ${this.getTimestamp()}`);
+  get salary(): number {
+    if (!this._salary) {
+      throw new Error('Salary is not set');
+    }
+    return this._salary;
+  }
+
+  public static getCompanyName(): string {
+    return Employee.companyName;
+  }
+
+  static {
+    Employee.companyName = 'Adobe';
+  }
+
+  public getDetails(): string {
+    return `Name: ${this.name} Age: ${this.age}`;
   }
 }
 
-const user: UserWithTimestamp = new UserWithTimestamp('Alice', 30);
-console.log(user);
-user.displayInfo();
+class Manager extends Employee {
+  constructor(
+    name: string,
+    age: number,
+    id: number,
+    public department: string,
+  ) {
+    super(name, age, id);
+  }
+
+  public getDetails(): string {
+    return `Name: ${this.name} Age: ${this.age} Department: ${this.department}`;
+  }
+}
+
+const employee: Employee = new Employee('John', 32, 1);
+employee.salary = 10000;
+console.log(employee.getDetails());
+console.log(employee.salary);
+
+const manager: Manager = new Manager('Alice', 22, 1, 'IT');
+manager.salary = 20000;
+console.log(manager.getDetails());
+console.log(manager.salary);
