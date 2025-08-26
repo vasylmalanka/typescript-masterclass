cf2fb0797941b0cd3c8a988a18c6c724477252a7
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-26 22:41:35 +0200

    9.104

--- index.ts
+++ index.ts
@@ -1,65 +1,17 @@
-class Employee {
-  private static companyName: string;
+type Holidays = {
+  date: Date;
+  reason: string;
+}[];
 
-  protected readonly id: number;
-  private _salary: number | undefined;
-
-  constructor(
-    public readonly name: string,
-    public readonly age: number,
-    id: number,
-  ) {
-    this.id = id;
-  }
-
-  set salary(salary: number) {
-    if (salary < 0) {
-      throw new Error('Salary must be a positive number');
-    }
-    this._salary = salary;
-  }
-
-  get salary(): number {
-    if (!this._salary) {
-      throw new Error('Salary is not set');
-    }
-    return this._salary;
-  }
-
-  public static getCompanyName(): string {
-    return Employee.companyName;
-  }
-
-  static {
-    Employee.companyName = 'Adobe';
-  }
-
-  public getDetails(): string {
-    return `Name: ${this.name} Age: ${this.age}`;
-  }
+abstract class Department {
+  protected abstract holidays: Holidays;
+  protected constructor(protected name: string) {}
 }
 
-class Manager extends Employee {
-  constructor(
-    name: string,
-    age: number,
-    id: number,
-    public department: string,
-  ) {
-    super(name, age, id);
-  }
-
-  public getDetails(): string {
-    return `Name: ${this.name} Age: ${this.age} Department: ${this.department}`;
-  }
+class ItDepartment extends Department {
+  protected holidays: Holidays = [];
 }
 
-const employee: Employee = new Employee('John', 32, 1);
-employee.salary = 10000;
-console.log(employee.getDetails());
-console.log(employee.salary);
-
-const manager: Manager = new Manager('Alice', 22, 1, 'IT');
-manager.salary = 20000;
-console.log(manager.getDetails());
-console.log(manager.salary);
+class AdminDepartment extends Department {
+  protected holidays: Holidays = [];
+}
