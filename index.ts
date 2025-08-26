c67bc6d7539da162327a1f18dc78d7f8b9144035
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-26 22:44:14 +0200

    9.105

--- index.ts
+++ index.ts
@@ -6,6 +6,14 @@ type Holidays = {
 abstract class Department {
   protected abstract holidays: Holidays;
   protected constructor(protected name: string) {}
+
+  public addHolidays(holidays: Holidays) {
+    if (Array.isArray(holidays)) {
+      for (const holiday of holidays) {
+        this.holidays.push(holiday);
+      }
+    }
+  }
 }
 
 class ItDepartment extends Department {
