ae8cff9c9d6095f8b6407e3cbae5f3b1d6f069d6
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-27 14:51:15 +0200

    9.121

--- index.ts
+++ index.ts
@@ -1,3 +1,13 @@
+class User {
+  constructor(public name: string) {}
+}
+
+class Password {
+  constructor(public password: string) {}
+}
+
+class RegisteredUser extends User, Password {}
+
 enum AutomobileTypes {
   car = 'car',
   bus = 'bus',
