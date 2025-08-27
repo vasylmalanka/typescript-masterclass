51ffc314ae91e8866d949706f97cf3d783a03585
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-27 15:04:52 +0200

    9.124

--- index.ts
+++ index.ts
@@ -1,12 +1,35 @@
-interface IUser {
-  id: number;
+type User = {
   name: string;
 }
 
-interface IUser {
-  passwordHash: string;
+type AdminUser = {
+  isAdmin: boolean;
 }
 
-class User implements IUser {
-  constructor(public id: number, public name: string) {}
+// Intersection Type
+const userAndAdmin: User & AdminUser = {
+  name: 'John',
+  isAdmin: true,
+};
+
+// Union type
+const userOrAdmin: User | AdminUser = {
+  name: 'Mark',
+};
+
+// Tuples
+type ResponseTuple = [string, number];
+
+interface Name {
+  name: string;
 }
+
+interface LastName {
+  lastName: string;
+}
+
+class Person implements Name, LastName {
+  constructor(public name: string, public lastName: string) {}
+}
+
+const person: Person = new Person('John', 'Doe');
