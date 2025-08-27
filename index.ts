f6f99f17859dbdf58077db511d2cb6a445eb2215
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-27 14:00:17 +0200

    9.111

--- index.ts
+++ index.ts
@@ -1,92 +1,40 @@
-type Holidays = {
-  date: Date;
-  reason: string;
-}[];
-
-abstract class Department {
-  protected abstract holidays: Holidays;
-  protected constructor(protected name: string) {}
-
-  public addHolidays(holidays: Holidays) {
-    if (Array.isArray(holidays)) {
-      for (const holiday of holidays) {
-        this.holidays.push(holiday);
-      }
-    }
-  }
-
-  public abstract printHolidays(): void
+interface User {
+  userName: string;
+  email: string;
+  login(): void;
 }
 
-class ItDepartment extends Department {
-  protected holidays: Holidays = [];
+class Admin implements User {
+  constructor(
+    public userName: string,
+    public email: string,
+    public adminLevel: number,
+  ) {}
 
-  constructor() {
-    super('IT Department');
-  }
-
-  public printHolidays() {
-    if (this.holidays.length === 0) {
-      console.log('There are no holidays');
-    }
-    console.log(`Here is the list of holidays ${this.name}`);
-
-    this.holidays.forEach((holiday, index) => {
-      console.log(
-        `${index + 1}. ${holiday.reason}, ${holiday.date}`
-      );
-    });
+  public login(): void {
+    console.log('Admin is now logged in');
   }
 }
 
-class AdminDepartment extends Department {
-  protected holidays: Holidays = [];
+class Customer implements User {
+  constructor(
+    public userName: string,
+    public email: string,
+  ) {}
 
-  constructor() {
-    super('Admin Department');
-  }
-
-  public printHolidays() {
-    if (this.holidays.length === 0) {
-      console.log('There are no holidays');
-    }
-    console.log(`Here is the list of holidays ${this.name}`);
-
-    this.holidays.forEach((holiday, index) => {
-      console.log(
-        `${index + 1}. ${holiday.reason}, ${holiday.date}`
-      );
-    });
+  public login(): void {
+    console.log('User is now logged in');
   }
 }
 
-const itHolidays: Holidays = [
-  {
-    date: new Date (2022, 11, 1),
-    reason: 'IT Department Day',
-  },
-  {
-    date: new Date (2022, 12, 25),
-    reason: 'Christmas',
-  },
-];
+class Auth {
+  public static login(user: User) {
+    user.login();
+  }
+}
 
-const adminHolidays: Holidays = [
-  {
-    date: new Date (2022, 10, 1),
-    reason: 'Admin Department Day',
-  },
-  {
-    date: new Date (2022, 12, 25),
-    reason: 'Christmas',
-  },
-];
+const admin: Admin = new Admin('mark', 'mark@email.com', 1);
+const customer: Customer = new Customer('john', 'john@email.com');
 
-const itDepartment: ItDepartment = new ItDepartment();
-const adminDepartment: AdminDepartment = new AdminDepartment();
-
-itDepartment.addHolidays(itHolidays);
-adminDepartment.addHolidays(adminHolidays);
-
-itDepartment.printHolidays();
-adminDepartment.printHolidays();
+Auth.login(admin);
+Auth.login(customer);
