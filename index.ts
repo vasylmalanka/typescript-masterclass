7807506012f7111ebf4ab148c98ca63412300aef
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-27 14:10:28 +0200

    9.112

--- index.ts
+++ index.ts
@@ -1,40 +1,13 @@
-interface User {
-  userName: string;
+interface Person {
+  name: string;
   email: string;
-  login(): void;
+  age: number;
+  phone?: number;
+  greet?: () => void;
 }
 
-class Admin implements User {
-  constructor(
-    public userName: string,
-    public email: string,
-    public adminLevel: number,
-  ) {}
-
-  public login(): void {
-    console.log('Admin is now logged in');
-  }
-}
-
-class Customer implements User {
-  constructor(
-    public userName: string,
-    public email: string,
-  ) {}
-
-  public login(): void {
-    console.log('User is now logged in');
-  }
-}
-
-class Auth {
-  public static login(user: User) {
-    user.login();
-  }
-}
-
-const admin: Admin = new Admin('mark', 'mark@email.com', 1);
-const customer: Customer = new Customer('john', 'john@email.com');
-
-Auth.login(admin);
-Auth.login(customer);
+const person: Person = {
+  name: 'john',
+  email: 'john@email.com',
+  age: 32,
+};
