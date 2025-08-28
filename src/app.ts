f27af427aa94594948c5afe2c697f8a639e5fb62
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-28 15:18:31 +0200

    10.134

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,8 @@
-const button = document.getElementById('button');
+interface Keyboard {
+  type: string;
+  modelID: number;
+}
+
+const createKeyboard = (modelID: number): Keyboard => {
+  return { type: 'Keyboard', modelID }
+}
