9659a33eb24ed855cc8b7e9305ef6e61941b3628
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-29 20:00:18 +0200

    11.142

--- main.js
+++ main.js
@@ -26,10 +26,19 @@ function User (name, email) {
   this.name = name;
   this.email = email;
   this.points = 0;
+}
 
-  this.login = () => { console.log(this.name, 'Has logged in'); };
-  this.logout = () => { console.log(this.name, 'Has logged out'); };
-  this.addPoint = () => { console.log('total points', this.points); };
+User.prototype.login = function () {
+  console.log(this.name, 'Has logged in');
+}
+
+User.prototype.logout = function () {
+  console.log(this.name, 'Has logged out');
+}
+
+User.prototype.addPoint = function () {
+  this.points++;
+  console.log('total points', this.points);
 }
 
 const user = new User('John', 'john@email.com');
