81c18943c69c7cd7ab3894258568d1cf21690750
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-29 19:55:32 +0200

    11.141

--- main.js
+++ main.js
@@ -1,26 +1,37 @@
-class User {
-  constructor(name, email) {
-    this.name = name;
-    this.email = email;
-    this.points = 0;
-  }
+// class User {
+//   constructor(name, email) {
+//     this.name = name;
+//     this.email = email;
+//     this.points = 0;
+//   }
 
-  login() {
-    console.log(this.name, 'Has logged in');
-  }
+//   login() {
+//     console.log(this.name, 'Has logged in');
+//   }
 
-  logout() {
-    console.log(this.name, 'Has logged out');
-  }
+//   logout() {
+//     console.log(this.name, 'Has logged out');
+//   }
 
-  addPoint() {
-    this.points++;
-    console.log('total points', this.points);
-  }
+//   addPoint() {
+//     this.points++;
+//     console.log('total points', this.points);
+//   }
+// }
+
+// const user = new User('John', 'john@email.com');
+// console.log(user);
+
+function User (name, email) {
+  this.name = name;
+  this.email = email;
+  this.points = 0;
+
+  this.login = () => { console.log(this.name, 'Has logged in'); };
+  this.logout = () => { console.log(this.name, 'Has logged out'); };
+  this.addPoint = () => { console.log('total points', this.points); };
 }
 
 const user = new User('John', 'john@email.com');
-const user2 = new User('Mark', 'mark@email.com');
 
 console.log(user);
-user2.addPoint();
