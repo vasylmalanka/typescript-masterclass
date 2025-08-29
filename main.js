688babea4885203bb13a60d2f7d8527c1f4f7607
Vasyl Malanka <vsl.malanka@gmail.com>
2025-08-29 20:17:10 +0200

    11.146

--- main.js
+++ main.js
@@ -1,60 +1,12 @@
-// class User {
-//   constructor(name, email) {
-//     this.name = name;
-//     this.email = email;
-//     this.points = 0;
-//   }
-
-//   login() {
-//     console.log(this.name, 'Has logged in');
-//   }
-
-//   logout() {
-//     console.log(this.name, 'Has logged out');
-//   }
-
-//   addPoint() {
-//     this.points++;
-//     console.log('total points', this.points);
-//   }
-// }
-
-// const user = new User('John', 'john@email.com');
-// console.log(user);
-
-function User (name, email) {
-  this.name = name;
-  this.email = email;
-  this.points = 0;
-}
-
-User.prototype.login = function () {
-  console.log(this.name, 'Has logged in');
-}
-
-User.prototype.logout = function () {
-  console.log(this.name, 'Has logged out');
-}
-
-User.prototype.addPoint = function () {
-  this.points++;
-  console.log('total points', this.points);
-}
-
-function AdminUser(name, email, peopleReporting) {
-  User.apply(this, [name, email]);
-  this.peopleReporting = peopleReporting;
-}
-
-AdminUser.prototype = Object.create(User.prototype);
-
-AdminUser.prototype.updatePeopleReporting = function(newNumber) {
-  this.peopleReporting = newNumber;
+const book = {
+  title: 'the title',
+  pages: 300,
+  author: 'John',
 };
 
-const user = new User('John', 'john@email.com');
+const book2 = new Object();
+book2.title = 'Book2 title';
+book2.pages = 250;
+book2.author = 'Mark';
 
-const admin = new AdminUser('Mark', 'mark@email.com', 10);
-
-console.log(admin);
-console.log(user);
+console.log(book2);
