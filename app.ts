9c60bbd071b2a522d473d891226354c1b0c6b604
Vasyl Malanka <v.malanka@comwrap.reply.com>
2025-09-16 16:02:48 +0200

    12.157

--- app.ts
+++ app.ts
@@ -1,43 +1,31 @@
-function methodLogger<This, Args extends any[], Return>(logPrefix: string) {
-  return function (
-    target: (this: This, ...args: Args) => Return,
-    _context: ClassMethodDecoratorContext<
-      This,
-      (this: This, ...args: Args) => Return
-    >
-  ) {
-    return function (this: This, ...args: Args) {
-      console.log(`${logPrefix} Inovocation Started`);
-      target.call(this, ...args);
-      console.log(`${logPrefix} Inovocation Ended`);
-    }
+function addDefaultPost<This, Value extends Post[]>(
+  _target: undefined,
+  _context: ClassFieldDecoratorContext<This, Value>
+) {
+  return function (initialValue: Value) {
+    initialValue.push({
+      title: 'Default Title',
+      content: 'Default Content',
+    });
+    return initialValue;
   }
 }
 
-function bound(_target: Function, context: ClassMethodDecoratorContext) {
-  const methodName = String(context.name);
-
-  if (context.private) {
-    throw new Error('Cannot decorate private property');
-  }
-
-  context.addInitializer(function (this: any) {
-    this[methodName] = this[methodName].bind(this);
-  });
+type Post = {
+  title: string;
+  content: string;
 }
 
-class Person {
+class Author {
+  @(addDefaultPost<Author, Post[]>)
+  public posts: Post[] = [];
+
   constructor(public name: string) {}
 
-  @bound
-  @methodLogger<Person, [string], void>('LOG: ')
   greet(greeting: string): void {
     console.log(`${greeting}, ${this.name}`);
   }
 }
 
-let user: Person = new Person('John');
-user.greet('Hello');
-
-const greet = user.greet;
-greet('Hi');
+const author = new Author('Mark');
+console.log(author.posts);
