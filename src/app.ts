89619080dc09e4e3b4028430d51265d0b9e9f35d
Vasyl <v.malanka@comwrap.reply.com>
2025-12-27 11:22:20 +0100

    12.167

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,24 @@
-function FirstDecorator(name: string) {
-  return function (target: Function) {
-    console.log(`${name} Invoked`);
-    console.log(target);
+enum Manufacturers {
+  boeing = 'boeing',
+  airbus = 'airbus',
+}
+
+function AircraftManufacturer(manufacturer: Manufacturers) {
+  return (target: Function) => {
+    if (manufacturer === Manufacturers.airbus) {
+      target.prototype.origin = 'United States of America';
+      target.prototype.manufacturer = Manufacturers.airbus;
+      target.prototype.type = 'Jet';
+    } else {
+      target.prototype.origin = 'France';
+      target.prototype.manufacturer = Manufacturers.boeing;
+      target.prototype.type = 'Helicopter';
+    }
   }
 }
 
-@FirstDecorator('First Decorator')
-class Aircraft {
+@AircraftManufacturer(Manufacturers.airbus)
+class Airplane {
   constructor(
     public _aircraftModel: string,
     private pilot: string
@@ -21,4 +33,6 @@ class Aircraft {
   }
 }
 
-new Aircraft('Airbus A380', 'John');
+const airplane = new Airplane('Airbus A380', 'John');
+//@ts-ignore
+console.log(airplane.manufacturer);
