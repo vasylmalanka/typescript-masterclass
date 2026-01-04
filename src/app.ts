a9a819a1bcd2a6269ce41de46e1561d5fb6a39aa
Vasyl <v.malanka@comwrap.reply.com>
2026-01-04 19:03:26 +0100

    12.181

--- src/app.ts
+++ src/app.ts
@@ -1,40 +1,9 @@
-interface Ball {
-  diameter: number;
+const welcomeString = 'Hello There';
+let replyString = 'Hey';
+
+let unionString: string | undefined;
+unionString.length;
+
+if (unionString) {
+  unionString.length;
 }
-
-interface Sphere {
-  diameter: number;
-}
-
-let ball: Ball = { diameter: 10 };
-let sphere: Sphere = { diameter: 20 };
-
-sphere = ball;
-ball = sphere;
-
-interface Tube {
-  diameter: number;
-  length: number;
-}
-
-let tube: Tube = {
-  diameter: 12,
-  length: 3,
-}
-
-ball = tube;
-// tube = ball;
-
-type ValidatedInputString = string & { __brand: 'Validated Input' };
-
-const validateUserInput = (input: string) => {
-  const simpleValidatedInput = input.trim();
-  return simpleValidatedInput as ValidatedInputString;
-};
-
-const printName = (name: ValidatedInputString) => {
-  console.log(name);
-}
-
-printName(validateUserInput('John'));
-printName('Mark');
