85223e875da54c99865c81c23fc859e4d71560b4
Vasyl <v.malanka@comwrap.reply.com>
2026-01-06 11:00:32 +0100

    15.185

--- src/app.ts
+++ src/app.ts
@@ -1,32 +1,8 @@
-type Circle = {
-  kind: 'circle';
-  radius: number;
+// Mapped Types
+// Partial Type Implementation
+type PartialType<T> = {
+  [P in keyof T]?: T[P];
 };
-
-type Square = {
-  kind: 'square';
-  side: number;
-};
-
-type Rectangle = {
-  kind: 'rectangle';
-  length: number;
-  breadth: number;
-};
-
-type Shape = Circle | Square | Rectangle;
-
-function getArea (shape: Shape): number
-{
-  switch (shape.kind) {
-    case 'circle':
-      return Math.PI * shape.radius ** 2;
-    case 'square':
-      return shape.side ** 2;
-    case 'rectangle':
-      return shape.length * shape.breadth;
-    default:
-      const _exhaustiveCheck: never = shape;
-      throw new Error('Unhandled shape type');
-  }
-}
+// Conditional Types
+// Exclude Type Implementation
+type ExcludeType<T, U> = T extends U ? never : T;
