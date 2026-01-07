cd70309e316dc81b7f067d8d9316f33583e31f37
Vasyl <v.malanka@comwrap.reply.com>
2026-01-07 15:48:37 +0100

    15.187

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,23 @@
-const promise: Promise<number> = new Promise ((res, _rej) => {
-  setTimeout(() => {
-    res(1);
-  }, 1000);
-});
+type Roles = 'author' | 'editor' | 'researcher';
 
-type AwaitedType = Awaited<typeof promise>;
+interface User {
+  name: string;
+  email: string;
+  age: number;
+};
+
+interface Article {
+  title: string;
+  content: string;
+  contributors: Record<Roles, User>;
+}
+
+const article: Article = {
+  title: 'Lorem',
+  content: 'Duis',
+  contributors: {
+    author: { name: 'John', email: 'john@email.com', age: 32 },
+    editor: { name: 'Frank', email: 'frank@email.com', age: 36 },
+    researcher: { name: 'Mark', email: 'mark@email.com', age: 36 },
+  },
+};
