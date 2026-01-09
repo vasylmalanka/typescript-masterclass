69244f698bdadabaea8956ed6d01222a3bb3dcbf
Vasyl <v.malanka@comwrap.reply.com>
2026-01-09 16:08:18 +0100

    15.191

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,8 @@
 interface User {
-  name: string;
-  email: string;
-  password: string;
+  name?: string;
+  age?: number;
+  email?: string;
+  password?: string;
 }
 
-function updateUser(user: User, updates: Partial<User>): User {
-  return {...user, ...updates};
-}
-
-const user: User = {
-  name: 'John',
-  email: 'john@site.com',
-  password: 'password',
-};
-
-const updatedUser = updateUser(user, { email: 'john@email.com' });
-console.log(updatedUser);
+type RegisterUser = Required<Pick<User, 'email' | 'password'>>;
