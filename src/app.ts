90283caf3abd71fbbcd91fc63650e98dfe71b027
Vasyl <v.malanka@comwrap.reply.com>
2026-01-13 11:34:38 +0100

    15.194

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,16 @@
-type City = 'new york' | 'london' | 'tokyo';
-type UppercaseCity = Uppercase<City>;
-type LowercaseCity = Lowercase<UppercaseCity>;
-type CapitalizeCity = Capitalize<City>;
-type UncapitalizeCity = Uncapitalize<CapitalizeCity>;
+type Properties = 'red' | 'green' | 'blue';
+type RGB = [red: number, green: number, blue: number];
+
+const color = {
+  red: [255, 0, 0],
+  green: '#00ff00',
+  blue: [255, 255, 0],
+} satisfies Record<Properties, RGB | string>;
+
+const redComponent = color.red[0]; // 255
+
+const greenValue = color.green.toUpperCase();
+// '#00FF00'
+
+const blueValue = color.blue.toUpperCase();
+// Property 'toUpperCase' does not exist on type '[number, number, number]'.
