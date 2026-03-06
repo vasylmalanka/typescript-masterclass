7155408c8eced86c99e92d15fa7c9a179610f435
Vasyl <v.malanka@comwrap.reply.com>
2026-03-06 16:47:26 +0100

    16.201

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,16 @@
+type IsString<T> = T extends string ? 'Yes' : 'No';
+
+type Test1 = IsString<string>;
+type Test2 = IsString<number>;
+
+type OptionIfString<T> = {
+    [K in keyof T]: T[K] extends string ? T[K] | undefined : T[K];
+};
+
 type User = {
     id: number;
     name: string;
     age: number;
 };
 
-type PartialUser = {
-    [K in keyof User]?: User[K];
-}
+type OptionalNameUser = OptionIfString<User>;
