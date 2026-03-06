c5c60950b5287b3202e64667d2b5e274b40e5d1c
Vasyl <v.malanka@comwrap.reply.com>
2026-03-06 16:39:18 +0100

    16.200

--- src/app.ts
+++ src/app.ts
@@ -4,12 +4,6 @@ type User = {
     age: number;
 };
 
-type UserNameType = User['name'];
-type UserIDType = User['id'];
-
-function getProperty<T, K extends keyof T>(obj: T, key: K): T[K] {
-    return obj[key];
+type PartialUser = {
+    [K in keyof User]?: User[K];
 }
-const userData: User = { id: 1, name: 'Bob', age: 25 };
-
-const userName: User['name'] = getProperty(userData, 'name');
