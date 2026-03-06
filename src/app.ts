8f571f6d096aa3f7435548ff62bfb2b84d933731
Vasyl <v.malanka@comwrap.reply.com>
2026-03-06 17:31:39 +0100

    17.205

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,22 @@
-function printStrings(str: string | string[] | null) {
-    if (str && typeof str === 'object') {
-        for (const s of str) {
-            console.log(s);
-        }
-    } else if (str === 'string') {
-        console.log(str);
-    } else {
-        console.log('Pass an array of strings or a string as an argument');
-    }
+type Person = {
+  name: string;
+  age?: number;
+};
+
+function printAge(person: Person) {
+  if (person.age) {
+    console.log(person.age);
+  } else {
+    console.log('Age unknown');
+  }
 }
+
+const user: Person = {
+  name: 'John',
+  age: 'John'
+}
+
+printAge({
+  name: 'John',
+  age: 'John'
+});
