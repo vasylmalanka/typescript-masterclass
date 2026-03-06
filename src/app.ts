8747609c087694676722f98dcb9d477c6905432a
Vasyl <v.malanka@comwrap.reply.com>
2026-03-06 17:40:37 +0100

    17.206

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,19 @@
-type Person = {
-  name: string;
-  age?: number;
+type Circle = {
+  kind: 'circle';
+  radius: number;
 };
 
-function printAge(person: Person) {
-  if (person.age) {
-    console.log(person.age);
+type Square = {
+  kind: 'square';
+  sideLength: number;
+}
+
+type Shape = Circle | Square;
+
+function getArea (shape: Shape) {
+  if (shape.kind === 'circle') {
+    return Math.PI * shape.radius ** 2;
   } else {
-    console.log('Age unknown');
+    return shape.sideLength ** 2;
   }
 }
-
-const user: Person = {
-  name: 'John',
-  age: 'John'
-}
-
-printAge({
-  name: 'John',
-  age: 'John'
-});
