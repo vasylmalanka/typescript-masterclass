38f52d685e57c9447829f54a6af61a97c334e2c5
Vasyl <v.malanka@comwrap.reply.com>
2026-03-27 21:34:09 +0100

    23.235-fix

--- src/config/container.ts
+++ src/config/container.ts
@@ -1,6 +1,6 @@
 import { Container } from 'inversify';
-import { TasksController } from '../../tasks/tasks.controller.js';
-import { TasksRouter } from '../../tasks/tasks.router.js';
+import { TasksController } from '../tasks/tasks.controller.js';
+import { TasksRouter } from '../tasks/tasks.router.js';
 
 export const container: Container = new Container();
 
--- src/config/routes.config.ts
+++ src/config/routes.config.ts
@@ -1,6 +1,6 @@
 import type { Application } from 'express';
 import { container } from './container.js';
-import { TasksRouter } from '../../tasks/tasks.router.js';
+import { TasksRouter } from '../tasks/tasks.router.js';
 
 export function addRoutes(app: Application): Application {
   const tasksRouter = container.get<TasksRouter>(TasksRouter);
