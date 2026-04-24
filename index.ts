4e4487c06b124bdfdc97199d0775d02f340cd6c4
Vasyl <v.malanka@comwrap.reply.com>
2026-04-24 12:18:55 +0200

    24.240

--- index.ts
+++ index.ts
@@ -2,12 +2,37 @@ import 'reflect-metadata';
 
 import express, { type Express } from 'express';
 import { addRoutes } from './src/config/routes.config.js';
+import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 const app: Express = express();
-const port = 3001;
+const port = process.env.PORT;
 
 addRoutes(app);
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+async function bootstrap() {
+  if (!process.env.DATABASE_URL || !process.env.DATABASE_NAME) {
+    throw new Error('Cannot read environment variables');
+    process.exit(1);
+  }
+
+  try {
+    await mongoose.connect(
+      process.env.DATABASE_URL,
+      {
+        dbName: process.env.DATABASE_NAME,
+      }
+    );
+    console.log('Connected to MongoDB');
+    app.listen(port, () => {
+      console.log(`Server is running at http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
+}
+
+bootstrap();
