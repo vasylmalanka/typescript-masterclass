2bc4b41d769b8e207d672486bb7f28d5eff402eb
Vasyl <v.malanka@comwrap.reply.com>
2026-05-02 21:25:16 +0200

    25.255

--- src/config/container.ts
+++ src/config/container.ts
@@ -2,9 +2,11 @@ import { Container } from 'inversify';
 import { TasksController } from '../tasks/tasks.controller.js';
 import { TasksRouter } from '../tasks/tasks.router.js';
 import { UserController } from '../user/user.controller.js';
+import { TaskService } from '../tasks/tasks.service.js';
 
 export const container: Container = new Container();
 
 container.bind(TasksController).toSelf().inTransientScope();
 container.bind(TasksRouter).toSelf().inTransientScope();
 container.bind(UserController).toSelf().inTransientScope();
+container.bind(TaskService).toSelf().inTransientScope();
--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,29 +1,28 @@
 import { injectable, inject } from 'inversify';
-import { UserController } from '../user/user.controller.js';
-import { Task } from './task.schema.js';
 import type { Request, Response } from 'express';
 import type { IPartialTaskWithId, ITask } from './task.interface.js';
 import { Document } from 'mongoose';
+import { TaskService } from './tasks.service.js';
 
 @injectable()
 export class TasksController {
-  constructor(@inject(UserController) private userController: UserController) {}
+  constructor(
+    @inject(TaskService) private taskService: TaskService,
+  ) {}
 
   public async handleGetTasks(req: Request, res: Response) {
-    const tasks = await Task.find();
-
+    const tasks = await this.taskService.findAll();
     return tasks;
   }
 
   public async handlePostTasks(req: Request<{}, {}, ITask>, res: Response) {
-    const task: Document<unknown, any, ITask> = new Task(req.body);
+    const task: Document<unknown, any, ITask> = await this.taskService.createTask(req.body);
     await task.save();
-
     return task;
   }
 
   public async handlePatchTasks(req: Request<{}, {}, IPartialTaskWithId>, res: Response) {
-    const task = await Task.findById(req.body._id);
+    const task = await this.taskService.findById(req.body._id);
     if (task) {
       task.title = req.body.title ? req.body.title : task.title;
       task.description = req.body.description ? req.body.description : task.description;
