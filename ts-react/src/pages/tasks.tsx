68fd94e75e286494f767b49b6494130ddc6f8df9
Vasyl <v.malanka@comwrap.reply.com>
2026-05-12 16:35:34 +0200

    29.271

--- ts-react/src/pages/tasks.tsx
+++ ts-react/src/pages/tasks.tsx
@@ -1,5 +1,21 @@
 import type { FC, ReactElement } from "react";
 
 export const Tasks: FC = (): ReactElement => {
-  return <h1>Tasks Page</h1>;
+  return (
+    <section className="flex flex-row w-full p-4 gap-8">
+      <section className="flex basis-2/3 justify-center">
+        <div className="flex flex-col w-4/5 p-4">
+          <h1 className="text-white font-bold text-2xl mb-8">Tasks as on: Saturday, 1 Mar 2025</h1>
+          <div className="flex justify-around">
+            <p className="text-white">Counter 1</p>
+            <p className="text-white">Counter 2</p>
+            <p className="text-white">Counter 3</p>
+          </div>
+        </div>
+      </section>
+      <section className="flex basis-1/3 bg-pink-300">
+        Create Task Section
+      </section>
+    </section>
+  );
 };
